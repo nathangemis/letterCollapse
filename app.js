0c58631e4bb63e91305de545e31b7957f766ee50
nathangemis <nathangemis@gmail.com>
2018-10-28 22:42:12 +0100

    appjs maj

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let intervalTime = 1000;
 displayInterval = setInterval(displayLetter, intervalTime);
 let divSize = 150;
 let fontSize = 5;
+let totalCollapse = divSize;
 root.style.setProperty("--font-size", `${fontSize}em`)
 root.style.setProperty("--size", `${divSize}px`);
 let letterIndex = 0;
-let totalCollapse = 0;
 function displayLetter() {
     let wordCompleted = document.body.querySelector('.resume');
     if (wordCompleted) {
@@ -51,25 +51,38 @@ function letterComplete() {
 
 }
 content.addEventListener("mouseenter", function () {
-    collapseTime = 2000 / totalWidth;
-    collapseInterval = setInterval(collapse, 1);
+    totalCollapse = divSize;
+    startTimeOut = setTimeout(startInterval, 1000);
+
+    function startInterval() {
+        collapseInterval = setInterval(collapse, 1);
+    }
+
     function collapse() {
+        collapseTime = totalWidth / 100;
         if (totalCollapse < totalWidth) {
-            totalCollapse = totalCollapse * collapseTime;
+            totalCollapse = totalCollapse + collapseTime;
 
         } else {
+            totalCollapse = totalWidth;
             clearInterval(collapseInterval);
         }
         root.style.setProperty('--collapse', `${totalCollapse}px`);
+        console.log(totalCollapse);
     }
+
+
     clearInterval(displayInterval);
     wordCompleted = document.body.querySelector('.resume')
     wordCompleted.style.display = 'flex';
     content.classList.remove("hideResume");
 });
 content.addEventListener("mouseleave", function () {
+    clearTimeout(startTimeOut)
     clearInterval(collapseInterval);
     totalCollapse = 0;
     displayInterval = setInterval(displayLetter, 1000);
     content.classList.add('hideResume');
 });
+
+
